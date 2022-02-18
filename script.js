267475d04778da8c5628de3e60c92f2acab9e690
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-02-18 02:24:27 +0100

    still on string methods

--- script.js
+++ script.js
@@ -37,3 +37,54 @@
     return input.split()
     }
     console.log(spiltStr('booy  girl'));
+
+    //Fix capitalization in names 
+    const capitalNames = function(input) {
+        const lowerCase = input.toLowerCase() ;
+        return lowerCase[0].toUpperCase() + lowerCase.slice(1)
+        
+    }
+    console.log(capitalNames('ukaoha chizoba'));
+    
+    // compare emails
+    const emailComaprison = function(email,loginEmail) {
+        const convertLowercase = loginEmail.toLowerCase().trim() ;
+        if(convertLowercase === email) {
+        return convertLowercase 
+        } else {
+            return 'email does not match'
+        }
+
+    }
+    console.log(emailComaprison('ukaoha@gmail.com' , 'ukaoha@gmail.com')); 
+
+    
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below),
+aa and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+*/
+
+
+
+
+
+
+
+
