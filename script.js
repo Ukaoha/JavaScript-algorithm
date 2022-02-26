f6249bf4287997c2ef3f287fb9dec36e81fba8c7
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-02-26 22:36:12 +0100

    solved a challange on array methods

--- script.js
+++ script.js
@@ -233,3 +233,65 @@ country.forEach(function(value,key,map){
 }) 
 
 // forEach on sets
+const countrySide = new Set(['Nigeria', 'naira' , 'USA','Dollars','British','pounds']);
+countrySide.forEach(function(value,_,Set){
+  console.log(`${value} ${_}`);
+});
+
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+*/ 
+
+/*const checkDogs = function(dogsJulia,dogsKate) {
+  JuliaData.splice(0,5,'dog2','dog3')
+console.log(JuliaData);
+if(dogsJulia => 3) {
+  console.log('Dog is an adult' ); 
+} else {
+  console.log('dog is a puupy');
+}
+}
+checkDogs();*/
+
+
+const checkDogs = function(dogsJulia,dogsKate) {
+  const juliaDataCorreted = dogsJulia.slice();
+  juliaDataCorreted.splice(0,1);
+  juliaDataCorreted.splice(-2);
+  console.log(juliaDataCorreted);
+
+  const dogs = juliaDataCorreted.concat(dogsKate);
+  console.log(dogs);
+  dogs.forEach(function(dogs,i) {
+    if( dogs >= 3 ) {
+      console.log(`Dog number ${i+1} is an adult, and is ${dogs} years old`);
+    } else {
+      console.log(`"Dog number ${i+1} is still a puppy 🐶"`);
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3,]);
+
+// using array methods ': maps,filter and reduce 
+// map retuns a new array as a result of all operations on orignal array element
+
+
+
+
+
+
+
+
