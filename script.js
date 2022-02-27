002dd8c8eeaf0e6f55dbe744fe1b99625cf95586
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-02-27 23:42:58 +0100

    learnt map,filter and reduce method

--- script.js
+++ script.js
@@ -287,11 +287,56 @@ checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3,]);
 
 // using array methods ': maps,filter and reduce 
 // map retuns a new array as a result of all operations on orignal array element
+const account1 = {
+  owner: "chizoba Ukaoha",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+const account2 = {
+  owner: "Stanly Ibe",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+const account3 = {
+  owner: "Grace Johnson",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+const account4 = {
+  owner: "Aka Favor",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+};
+const accounts = [account1, account2, account3, account4];
+// filter
+const deposit = movements.filter(function (mov) {
+  return mov > 0 ;
+})
+console.log(movements);
+console.log(deposit);
+
+const naira = 500 ;
+const UsdTonaira = movements.map(function(mov){
+  return naira * mov;
+})
+console.log(UsdTonaira);
+
+const withdrawal = movements.filter(function(mov){
+  return mov < 0 ;
+})
+console.log(withdrawal);
+ 
+console.log(movements);
 
 
 
-
-
-
-
-
+// reduce method 
+const balance = movements.reduce(function(accmulator, current) {
+  console.log(`iteration ${accmulator}`);
+  
+    return accmulator + current
+},0)
+console.log(balance);
